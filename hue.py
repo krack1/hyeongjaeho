997b5bab00a6166bf184c91f9f65a9597c3b1e8e
HyeongJH <krack1@naver.com>
2015-08-12 16:43:42 +0900

    Update hue.py

--- hue.py
+++ hue.py
@@ -13,17 +13,9 @@ def on(light):
 	data = response.read()
 
 	if data[3:10] is "success":
-<<<<<<< HEAD
-		print 'y'
-        return True
-    else:
-        print 'n'
-        return False
-=======
 		return True
 	else:
 		return False
->>>>>>> 6397db82e94981ea535d2c131a1aea0b36df7cb5
 
 
 #Hue 끄기
@@ -31,18 +23,17 @@ def off(light):
 	conn.request("PUT","/api/newdeveloper/lights/"+str(light)+"/state", '{"on":false}')
 	response = conn.getresponse()
 	data = response.read()
-    
-    if data[3:10] is "success":
-        print 'okay'
+
+	if data[3:10] is "success":
+		return True
 	else:
-        print 'no'
+		return False
 
 #Hue의 saturation변화 0~255 0 흰색
 def putSat(light, sat):
 	saturation = {}
 	saturation['sat'] = sat
 	saturation = json.dumps(saturation)
-	print saturation
 
 	conn.request("PUT","/api/newdeveloper/lights/"+str(light)+"/state", saturation)
 	response = conn.getresponse()
@@ -52,7 +43,6 @@ def putSat(light, sat):
 		return True
 	else:
 		return False
-
 
 
 #Hue의 밝기 변화 0~255 
@@ -76,7 +66,6 @@ def putHue(light, hue):
 	color = {}
 	color['hue'] = hue
 	color = json.dumps(color)
-	print color
 
 	conn.request("PUT","/api/newdeveloper/lights/"+str(light)+"/state", color)
 	response = conn.getresponse()
@@ -86,6 +75,7 @@ def putHue(light, hue):
 		return True
 	else:
 		return False
+
 
 
 #Hue 상태 가져오기 json객체 작성중
@@ -103,8 +93,3 @@ def getState(light):
 	ret.append(data['state']['sat'])
 	
 	return ret	
-
-
-on(1)
-
-	
