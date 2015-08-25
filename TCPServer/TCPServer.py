716e667bbc076d17b1f607f9c304b321ddfc3ddc
root <root@hyeong.(none)>
2015-08-25 14:00:10 +0900

    up

--- TCPServer/TCPServer.py
+++ TCPServer/TCPServer.py
@@ -1,9 +1,22 @@
+#-*- coding: utf-8 -*-
 import socket
 import time
 import json
 import httplib
 import time
+import sys
 
+def on(light):
+	conn.request("PUT","/api/newdeveloper/lights/"+light+"/state",'{"on":true}')
+def off(light):
+	conn.request("PUT","/api/newdeveloper/lights/"+light+"/state",'{"on":false}')
+def putState(light, hue, sat, bri):
+	state = {}
+	state['hue'] = hue
+	state['sat'] = sat
+	state['bri'] = bri
+	state = json.dumps(state)
+	conn.request("PUT","/api/newdeveloper/lights/"+light+"/state", state)
 
 
 #create a coscket object
@@ -23,16 +36,38 @@ serversocket.bind((host, port))
 serversocket.listen(5)
 
 while True:
-	#establish a connection
-	clientsocket,addr = serversocket.accept()
-	data = clientsocket.recv(buf)
-	print (data)
+
+		#establish a connection
+		clientsocket,addr = serversocket.accept()
+		data = clientsocket.recv(buf)
+		print (data)
+
+		decoded = json.loads(data)
+		light = decoded['light']
+		hue = decoded['hue']
+		onled = decoded['on']
+		sat = decoded['sat']
+		bri = decoded['bri']
+		
+		print light
+		print type(light)
+		print onled
+		print hue
+		print sat
+		print bri
+					
+	
+		if onled is True:
+			conn = httplib.HTTPConnection("192.168.0.5")
+			on(light)
+			conn = httplib.HTTPConnection("192.168.0.5")
+			putState(light, hue, sat, bri)
+		else:
+
+			conn = httplib.HTTPConnection("192.168.0.5")
+			off(light)
 
 
-	decoded = json.loads(data)
-	print decoded['hue']
-	print decoded['on']
-	print decoded['sat']
-	print decoded['bri']
-	clientsocket.close()
+		clientsocket.close()
 
+serversocket.close()
